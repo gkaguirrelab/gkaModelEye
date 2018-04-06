41543e2a8f0424498ee8813a8b5309832c862f9d
gkaguirre <gkaguirre@me.com>
2018-04-06 14:45:14 -0400

    Integrate the ellipse intersect function into the ray trace func
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- rayTraceCenteredSurfaces.m
+++ rayTraceCenteredSurfaces.m
@@ -489,3 +489,73 @@ xp=opticalSystem(2)*cos(ang);
 yp=opticalSystem(3)*sin(ang);
 plot(opticalSystem(1)+xp,yp,'-k');
 end
+
+
+function [intersectionCoords, curvature] = calcEllipseIntersect(coordsInitial, theta, ellipseCenterZ, ellipseRadii )
+% Returns coords and curvature of an ellipse intersected by a ray 
+%
+% Syntax:
+%  [intersectionCoords, curvature] = calcEllipseIntersect(coordsInitial, theta, ellipseCenterZ, ellipseRadii )
+%
+% Description:
+%
+%
+% Inputs:
+%   coordsInitial         - 2x1 vector, with the values corresponding to
+%                           the z-position and height of the initial
+%                           position of the ray.
+%   theta                 - Scalar in radians. A value of zero is aligned
+%                           with the optical axis. Values between 0 and pi
+%                           direct the ray to diverge "upwards" away from
+%                           the axis.
+%   ellipseCenterZ        - Scalar in mm. The position of the center of the
+%                           elliptical surface on the z-axis
+%   ellipseRadii          - A 2x1 vector in mm, with the values
+%                           corresponding to the radii of the ellipitical
+%                           surface along the z and height axes,
+%                           respectively.
+%
+% Outputs:
+%   intersectionCoords    - 2x1 vector with the values corresponding to the
+%                           z-position and height at which the ray strikes
+%                           the elliptical surface
+%   curvature             - Scalar. The radius of curvature of the
+%                           elliptical surface at the point of intersection
+%                           with the ray.
+%
+% Examples:
+%{
+    [intersectCoords,curvature] = calcEllipseIntersect([-3.7,0], 0, -9.1412, [9.1412, 8.4277] )
+%}
+
+% Store the sign of the radius values.  They radii must have the same sign,
+% but we do not test for this here as it would interfere with symbolic
+% computations
+radiiSign = sign(ellipseRadii(1));
+% Convert the radii to their absolute values
+ellipseRadii = abs(ellipseRadii);
+
+% Convert the ray position and theat to the slope (m) and y-axis
+% intercept(c) of a line.
+m = tan(theta);
+c = coordsInitial(2)-m*coordsInitial(1);
+
+% Obtain the pair of x and y coordinates that the line will intersect
+% the ellipse
+M = (1/ellipseRadii(1)^2) + (1/ellipseRadii(2)^2)* m^2;
+N = 2*(1/ellipseRadii(2)^2)*m*c - 2*(1/ellipseRadii(1)^2)*ellipseCenterZ;
+O = (1/ellipseRadii(2)^2)*c^2 + (1/ellipseRadii(1)^2)*ellipseCenterZ^2 -1 ;
+determinant = (N^2 - 4* M * O);
+xCoords = [(- N + sqrt(determinant))/ (2*M), (- N - sqrt(determinant))/ (2*M)];
+yCoords = [m*xCoords(1) + c, m*xCoords(2) + c];
+
+% If the radiiSign is positive, report the coordinates on the left-hand
+% side of the ellipse, otherwise report the coordinates on the right
+intersectionCoords = [xCoords((radiiSign/2)+1.5),yCoords((radiiSign/2)+1.5)];
+
+% Calculate the radius of curvature at the point of intersection. If the
+% radiiSign is negative, report a negative radius of curvature
+t = acos((intersectionCoords(1)-ellipseCenterZ)/ellipseRadii(1));
+curvature = radiiSign*((ellipseRadii(1)^2*sin(t)^2 + ellipseRadii(2)^2*cos(t)^2)^(3/2))/(ellipseRadii(1)*ellipseRadii(2));
+
+end
