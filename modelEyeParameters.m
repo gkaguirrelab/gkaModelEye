9fd352a5b9c73128d2ef111c53c7d87c397b4a9c
gkaguirre <gkaguirre@me.com>
2018-05-10 09:47:50 -0400

    Organizing blocks of code
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- modelEyeParameters.m
+++ modelEyeParameters.m
@@ -257,12 +257,87 @@ switch p.Results.species
         end
 
         
+        %% Iris
+        % The iris has a thickness. This thickness influences the
+        % properties of the entrance pupil, as when the eye is rotated
+        % w.r.t. the camera either front or back surface of the iris
+        % aperture defines the near or far edge of the entrance pupil.
+        eye.iris.thickness = 0.15;
+
+        % We position the anterior surface of the iris at the depth of the
+        % anterior point of the lens. We model an eye with zero iris angle,
+        % thus making the iris a plane. We adjust the position of the iris
+        % so that it is centered within the rotated corneal ellipse. This
+        % is consistent with reports that the iris is shifted slightly
+        % upward with respect to the pupil center, although inconsistent
+        % with the report that it is shifted temporally:
+        %
+        %   ...the typical entrance pupil is decentered
+        %   approximately 0.15 mm nasally and 0.1 mm inferior to the
+        %   geometric center of the visible iris circumference
+        %
+        % Bennett, Edward S., and Barry A. Weissman, eds. Clinical contact
+        % lens practice. Lippincott Williams & Wilkins, 2005, p119
+        switch eyeLaterality
+            case 'Right'
+                eye.iris.center = [-4+eye.iris.thickness/2 0.35 0.35];
+            case 'Left'
+                eye.iris.center = [-4+eye.iris.thickness/2 -0.35 0.35];
+        end
+        
+        % Define the iris radius. One study measured the horizontal visible
+        % iris diameter (HVID) in 200 people, and found a mean of 11.8 with
+        % a range of 10.2 - 13.0.
+        %
+        %    PJ Caroline & MP Andrew. "The Effect of Corneal Diameter on
+        %    Soft Lens Fitting, Part 1" Contact Lens Spectrum, Issue: April
+        %    2002
+        %    https://www.clspectrum.com/issues/2002/april-2002/contact-lens-case-reports
+        %
+        % Bernd Bruckner of the company Appenzeller Kontaktlinsen AG
+        % supplied me with a tech report from his company (HVID & SimK
+        % study) that measured HVID in 461 people. These data yield a mean
+        % iris radius of 5.92 mm, 0.28 SD. The values from the histogram
+        % are represented here, along with a Gaussian fit to the
+        % distribution
+        %{
+            counts = [0 2 2 0 0 4 5 12 19 23 36 44 52 41 39 37 43 30 28 12 15 10 4 1 0 2 0];
+            HVIDRadiusmm = (10.5:0.1:13.1)/2;
+            hvidGaussFit = fit(HVIDRadiusmm', counts', 'gauss1');
+            hvidRadiusMean = hvidGaussFit.b1;
+            hvidRadiusSD =  hvidGaussFit.c1;
+            figure
+            plot(HVIDRadiusmm, hvidGaussFit(HVIDRadiusmm), '-r')
+            hold on
+            plot(HVIDRadiusmm, counts, '*k')
+            xlabel('HVID radius in mm')
+            ylabel('counts')
+        %}
+        % The HVID is the refracted iris size. We can use the forward model
+        % to find the size of the true iris.
+        %{
+            sceneGeometry = createSceneGeometry();
+            sceneGeometry.refraction = [];
+            % Get the area in pixels of a "pupil" that is the same radius
+            % as the HVID when there is no ray tracing
+            hvidP=pupilProjection_fwd([0 0 0 hvidRadiusMean],sceneGeometry);
+            % Restore ray tracing
+            sceneGeometry = createSceneGeometry();
+            % Set up the objective function
+            myArea = @(p) p(3);
+            myObj = @(r) (hvidP(3) - myArea(pupilProjection_fwd([0 0 0 r],sceneGeometry)))^2;
+            [r,pixelError] = fminsearch(myObj,5.5);
+            fprintf('An unrefracted iris radius of %4.2f yields a refracted HVID of %4.2f \n',r,hvidRadiusMean)
+        %}
+        % We use this true iris size and then subject the iris perimeter
+        % points to refraction
+        eye.iris.radius = 5.54;
+
+        
         %% Pupil
-        % We position the pupil plane at the depth of the anterior point of
-        % the lens, assuming cycloplegia. The coordinate space of the model
-        % eye is defined w.r.t. the center of the pupil, so the p2 and p3
-        % values are zero
-        eye.pupil.center = [-4 0 0];
+        % The pupil is an aperture in the iris, centered on the optical
+        % axis
+        eye.pupil.center = [eye.iris.center(1) 0 0];
         
         % The actual pupil of the eye is elliptical. Further, the
         % eccentricity and theta of the actual pupil ellipse changes with
@@ -361,81 +436,6 @@ switch p.Results.species
                 eye.pupil.thetas = [0  4/7*pi/2];
         end
         
-        
-        %% Iris
-        % Define the iris radius. One study measured the horizontal visible
-        % iris diameter (HVID) in 200 people, and found a mean of 11.8 with
-        % a range of 10.2 - 13.0.
-        %
-        %    PJ Caroline & MP Andrew. "The Effect of Corneal Diameter on
-        %    Soft Lens Fitting, Part 1" Contact Lens Spectrum, Issue: April
-        %    2002
-        %    https://www.clspectrum.com/issues/2002/april-2002/contact-lens-case-reports
-        %
-        % Bernd Bruckner of the company Appenzeller Kontaktlinsen AG
-        % supplied me with a tech report from his company (HVID & SimK
-        % study) that measured HVID in 461 people. These data yield a mean
-        % iris radius of 5.92 mm, 0.28 SD. The values from the histogram
-        % are represented here, along with a Gaussian fit to the
-        % distribution
-        %{
-            counts = [0 2 2 0 0 4 5 12 19 23 36 44 52 41 39 37 43 30 28 12 15 10 4 1 0 2 0];
-            HVIDRadiusmm = (10.5:0.1:13.1)/2;
-            hvidGaussFit = fit(HVIDRadiusmm', counts', 'gauss1');
-            hvidRadiusMean = hvidGaussFit.b1;
-            hvidRadiusSD =  hvidGaussFit.c1;
-            figure
-            plot(HVIDRadiusmm, hvidGaussFit(HVIDRadiusmm), '-r')
-            hold on
-            plot(HVIDRadiusmm, counts, '*k')
-            xlabel('HVID radius in mm')
-            ylabel('counts')
-        %}
-        % The HVID is the refracted iris size. We can use the forward model
-        % to find the size of the true iris.
-        %{
-            sceneGeometry = createSceneGeometry();
-            sceneGeometry.refraction = [];
-            % Get the area in pixels of a "pupil" that is the same radius
-            % as the HVID when there is no ray tracing
-            hvidP=pupilProjection_fwd([0 0 0 hvidRadiusMean],sceneGeometry);
-            % Restore ray tracing
-            sceneGeometry = createSceneGeometry();
-            % Set up the objective function
-            myArea = @(p) p(3);
-            myObj = @(r) (hvidP(3) - myArea(pupilProjection_fwd([0 0 0 r],sceneGeometry)))^2;
-            [r,pixelError] = fminsearch(myObj,5.5);
-            fprintf('An unrefracted iris radius of %4.2f yields a refracted HVID of %4.2f \n',r,hvidRadiusMean)
-        %}
-        % We use this true iris size and then subject the iris perimeter
-        % points to refraction
-        eye.iris.radius = 5.54;
-        
-        % We are aware of some reports that the iris is shifted slightly
-        % temporally and upward with respect to the pupil center:
-        %
-        %   ...the typical entrance pupil is decentered
-        %   approximately 0.15 mm nasally and 0.1 mm inferior to the
-        %   geometric center of the visible iris circumference
-        %
-        % Bennett, Edward S., and Barry A. Weissman, eds. Clinical contact
-        % lens practice. Lippincott Williams & Wilkins, 2005, p119
-        %
-        % We model an eye with zero iris angle, and thus set the depth of
-        % the iris plane equal to the pupil plane. We adjust the position
-        % of the iris so that it is centered within the rotated corneal
-        % ellipse.
-        switch eyeLaterality
-            case 'Right'
-                eye.iris.center = [eye.pupil.center(1) 0.35 0.35];
-            case 'Left'
-                eye.iris.center = [eye.pupil.center(1) -0.35 0.35];
-        end
-        
-        % The iris has a thickness, and this influences the size of the
-        % entrance pupil, as when the eye is rotated w.r.t. the camera the
-        % anterior and posterior edges of the iris are seen.
-        eye.iris.thickness = 0.15;
         
         %% Lens
         % Although the lens does not influence the pupil tracking, we
