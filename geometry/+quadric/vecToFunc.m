acede44db09f58465cb620bbac0e86d231c37c74
gkaguirre <gkaguirre@me.com>
2018-07-07 11:55:14 -0400

    accepting matrix or vec input
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- geometry/+quadric/vecToFunc.m
+++ geometry/+quadric/vecToFunc.m
@@ -9,6 +9,11 @@ function F = vecToFunc(v)
 % Note that the order of the cross-terms is xy, xz, yz
 %
 
+% If the quadric surface was passed in matrix form, convert to vec
+if isequal(size(v),[4 4])
+    v = quadric.matrixToVec(v);
+end
+
 F = @(xx,yy,zz) v(1)*xx.^2 + v(2)*yy.^2 + v(3)*zz.^2 + 2*v(4)*xx.*yy + 2*v(5)*xx.*zz + 2*v(6)*yy.*zz + 2*v(7)*xx + 2*v(8)*yy + 2*v(9)*zz + v(10);
 
 end
