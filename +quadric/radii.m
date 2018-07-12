186de42c8440d810f78191f689c05e6a676d63cd
gkaguirre <gkaguirre@me.com>
2018-07-12 11:29:53 -0400

    Radii are returned in canonical order
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- +quadric/radii.m
+++ +quadric/radii.m
@@ -22,27 +22,6 @@ r = 1./diag(sqrt(evals));
 sgns = sign( diag( evals ) );
 r = r .* sgns;
 
-% We now have the radii. We would like to return these values in the order
-% of the dimensions of the space (x, y, z). As a convention, the radius
-% along the (e.g.) x dimension is the radius that correspond to the axis of
-% the quadric surface that has an angle between -45 and 45 degrees w.r.t.
-% to the x axis.
-
-% Get the orientation
-thisOrientation = orientFxn(S);
-
-% % Define a mapping of orientations to radii order
-orientations = {[0 0 0],[0 0 1],[0 1 0],[1 0 0],[0 1 1],[1 0 1],[1 1 1]};
-orders = {[1 2 3],[1 3 2],[3 2 1],[2 1 3],[2 3 1],[3 1 2],[1 2 3]};
-thisOrder = orders{cellfun(@(x) isequal(x,thisOrientation),orientations)};
-
-% Report the radii in the specified order
-r = r(thisOrder);
 
 end
 
-
-function orientation = orientFxn(S)
-a = quadric.angles(S);
-orientation = mod(fix((abs(a)+45)./90),2);
-end
