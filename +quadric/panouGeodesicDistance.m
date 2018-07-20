2ee3ce3dfb57cd48cae317de93a89fc9c0015f77
gkaguirre <gkaguirre@me.com>
2018-07-20 12:53:34 -0400

    Working geodetic distance solution!
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- +quadric/panouGeodesicDistance.m
+++ +quadric/panouGeodesicDistance.m
@@ -1,87 +1,134 @@
-function [n,c,a0,a1,s] = panouGeodesicDistance(S,p1,p2)
-% Find the arc distance between two points on a quadric surface
+function [distance,startAngle,endAngle,geodeticPathCoords] = panouGeodesicDistance(S,X0,X1)
+% Find the geodesic distance between two points on a tri-axial ellipsoid
 %
 % Syntax:
-%  d = quadric.panouGeodesicDistance(S,p1,p2)
+%  [distance,startAngle,endAngle,geodeticPathCoords] = panouGeodesicDistance(S,X0,X1)
 %
 % Description:
-%   Returns the arc length distance on the quadric surface between two
-%   points. This currently only works for ellipsoids.
+%   Returns the geodesic distance between two points on the tri-axial
+%   ellipsoidal surface. The code was provided by Georgios Panou, and is
+%   based upon the approach described in:
 %
-% Reference: Panou G., 2013. The geodesic boundary value problem and its
-%            solution on a triaxial ellipsoid.
-%            Journal of Geodetic Science, 3 (3), 240-249.
+%       Panou, G. "The geodesic boundary value problem and its solution on
+%       a triaxial ellipsoid." Journal of Geodetic Science 3.3 (2013):
+%       240-249.
 %
 % Inputs:
 %   S                     - 4x4 quadric surface matrix
-%   p1, p2                - 3x1 vectors that specify the location of points
-%                           on the quadric surface.
+%   X0, X1                - 3x1 vectors that specify the Cartesian
+%                           location of points on the quadric surface.
 %
 % Outputs:
-%   d                     - Scalars. Arc length distance in word coordinate
-%                           units
-%   theta                 - Scalar in radians. Angle between p1 and p2
-%                           w.r.t. the center of the ellipsoid.
+%   distance              - Scalar. Distance of the geodetic between the
+%                           two points.
+%   startAngle, endAngle  - 
 %
 % Examples:
 %{
+    % Numeric example provided by G. Panou in the original code
+    S = quadric.scale(quadric.unitSphere,[0.015, 0.010, 0.009]);
+    X0 = quadric.ellipsoidalGeoToCart( [5 5 0], S );
+    X1 = quadric.ellipsoidalGeoToCart( [60 120 0], S );
+    [distance,startAngle,endAngle,geodeticPathCoords] = quadric.panouGeodesicDistance(S,X0,X1);
+    assert( max(abs(s - 0.0259)) < 1e-3 );
 
-    S = quadric.scale(quadric.unitSphere,[6378172, 6378103, 6356753]);
-    % Pick a point on the surface
-    G = [0; 0.5; 0];
-    X1 = quadric.ellipsoidalGeoToCart( G , S );
-    G = [80; 0.5; 0];
-    X2 = quadric.ellipsoidalGeoToCart( G , S );
-    quadric.arcLength(S,X1,X2);
+        F = quadric.vecToFunc(quadric.matrixToVec(S));
+    figure
+    [xx, yy, zz] = meshgrid( linspace(-2,2,100), linspace(-2,2,100), linspace(-2,2,100));
+    vertices = isosurface(xx, yy, zz, F(xx, yy, zz), 0);
+    p = patch(vertices);
+    p.FaceColor =[.9 .9 .9];
+    p.EdgeColor = 'none';
+    alpha(.8);
+    daspect([1 1 1])
+    view([40 30]); 
+    axis tight
+    axis equal
+    camlight
+    lighting gouraud
+    hold on
+    % Plot lines of varying beta
+    for beta = -90:10:90
+        coords =[];
+        for omega = -180:3:180
+            coords(end+1,:)=quadric.ellipsoidalGeoToCart( [beta, omega, 0], S );
+        end
+        plot3(coords(:,1),coords(:,2),coords(:,3),'-b');
+    end
+    fprintf('Lines of constant beta in blue\n');
+    % Plot lines of varying omega
+    for omega = -180:10:180
+        coords =[];
+        for beta = -90:3:90
+            coords(end+1,:)=quadric.ellipsoidalGeoToCart( [beta, omega, 0], S );
+        end
+        plot3(coords(:,1),coords(:,2),coords(:,3),'-g');
+    end
+    fprintf('Lines of constant omega in green\n');
+    plot3(geodeticPathCoords(:,1),geodeticPathCoords(:,2),geodeticPathCoords(:,3),'-r');
 %}
 
 
-% Examples:
-%{
-%----- Input -----
-% semi-axes ax >= ay >= b
-ax = 0.015
-ay = 0.010
-b = 0.009
-% ellipsoidal coordinates (in degrees e.g. 0(5,5), 1(60,120))
-beta0 = 5*(pi/180)
-lambda0 = 5*(pi/180)
-beta1 = 60*(pi/180)
-lambda1 = 120*(pi/180)
 % number of subspaces
-Sub = 20000
+nSubspaces = 20000;
+
 % desired accuracy (difference of beta1 in radians). For very small bodies,
 % may be larger, e.g. 10^(-9)
-epsilon = 10^(-12)
-%----------
-%}
+epsilonAccuracy = 10^(-12);
 
-% semi-axes ax >= ay >= b
-ax = 0.015
-ay = 0.010
-b = 0.009
-% ellipsoidal coordinates (in degrees e.g. 0(5,5), 1(60,120))
-beta0 = 5*(pi/180)
-lambda0 = 5*(pi/180)
-beta1 = 60*(pi/180)
-lambda1 = 120*(pi/180)
-% number of subspaces
-Sub = 20000
-% desired accuracy (difference of beta1 in radians). For very small bodies,
-% may be larger, e.g. 10^(-9)
-epsilon = 10^(-12)
+% resolution of the returned path coords
+pathResolution = 20;
 
-if lambda0 == lambda1
-    [n,c,a0,a1,s] = Geodesics_dbeta(ax,ay,b,beta0,lambda0,beta1,lambda1,Sub,epsilon)
-else
-    [n,c,a0,a1,s] = Geodesics(ax,ay,b,beta0,lambda0,beta1,lambda1,Sub,epsilon)
+% If the quadric surface was passed in vector form, convert to matrix
+if isequal(size(S),[1 10])
+    S = quadric.vecToMatrix(S);
 end
 
+% Obtain the radii of the quadric surface and distribute the values. We
+% adopt the canonical order of a => b => c, but the order returned after
+% alignment of the axes is a <= b <= c. This is why c is mapped to the
+% first value in the radii.
+radii = quadric.radii(quadric.alignAxes(S));
+a=radii(3);b=radii(2);c=radii(1);
+
+% Obtain the ellipsoidal geodetic coordinates for the two points, and
+% convert to radians
+G0 = deg2rad(quadric.cartToEllipsoidalGeo( X0, S ));
+G1 = deg2rad(quadric.cartToEllipsoidalGeo( X1, S ));
+
+if G0(2) == G1(2)
+    [nIterations,LiouvilleConstant,startAngle,endAngle,distance,geodeticPathCoords] = ...
+        Geodesics_dbeta(a,b,c,G0(1),G0(2),G1(1),G1(2),nSubspaces,epsilonAccuracy);
+else
+    [nIterations,LiouvilleConstant,startAngle,endAngle,distance,geodeticPathCoords] = ...
+        Geodesics(a,b,c,G0(1),G0(2),G1(1),G1(2),nSubspaces,epsilonAccuracy);
+end
+
+% Prepare a subset of the geodeticPathCoords to return
+sampleBase = round(linspace(1,size(geodeticPathCoords,1),pathResolution));
+geodeticPathCoords=geodeticPathCoords(sampleBase',:);
+
+% Re-arrange the order of the dimensions of the geodeticPathCoords so that
+% they reflect the axis order of the passed quadric surface
+geodeticPathCoords = geodeticPathCoords(:,[3 2 1]);
+
+% Rotate and translate the geodetic path coords so that they correspond to
+% the surface of the original quadric
+% Now counter-rotate and then counter-translate the X coordinate
+[~, rotMat] = quadric.alignAxes(S);
+for ii = 1:pathResolution
+    geodeticPathCoords(ii,:) = (geodeticPathCoords(ii,:)*rotMat');
+end
+
+origCenter = quadric.center(S);
+geodeticPathCoords = geodeticPathCoords+origCenter';
 
 end % panouGeodesicDistance
 
 
-function [n,c,a0,a1,s] = Geodesics_dbeta(ax,ay,b,beta0,lambda0,beta1,lambda1,Sub,epsilon)
+
+
+function [n,c,a0,a1,s,geodeticPathCoords] = Geodesics_dbeta(ax,ay,b,beta0,lambda0,beta1,lambda1,Sub,epsilon)
 
 %----- Ellipsoid -----
 hx = sqrt(ax^2-b^2);
@@ -105,36 +152,39 @@ ddlambda0 = 0;
 
 n=1;
 while n<100
-n;
-
-Dlambda0 = dlambda0;
-Ddlambda0 = ddlambda0;
-
-R = RK4_dbeta(beta0, beta1, Sub, [lambda0,Dlambda0+Ddlambda0,0,1], ax, ay, b);
-
-beta = R(:,1);
-lambda = R(:,2);
-dlambda = R(:,3);
-plambda = R(:,4);
-pdlambda = R(:,5);
-
-lambda10 = lambda(end);
-plambda10 = plambda(end);
-
-dlambda0 = dlambda(1);
-ddlambda0 = (lambda1-lambda10)/plambda10;
-
-%Check
-n = n+1;
-if abs(lambda1-lambda10) < epsilon
-   break
-end
+    n;
+    
+    Dlambda0 = dlambda0;
+    Ddlambda0 = ddlambda0;
+    
+    R = RK4_dbeta(beta0, beta1, Sub, [lambda0,Dlambda0+Ddlambda0,0,1], ax, ay, b);
+    
+    beta = R(:,1);
+    lambda = R(:,2);
+    dlambda = R(:,3);
+    plambda = R(:,4);
+    pdlambda = R(:,5);
+    
+    lambda10 = lambda(end);
+    plambda10 = plambda(end);
+    
+    dlambda0 = dlambda(1);
+    ddlambda0 = (lambda1-lambda10)/plambda10;
+    
+    %Check
+    n = n+1;
+    if abs(lambda1-lambda10) < epsilon
+        break
+    end
 end
 %----------
 
 x = ax.*sqrt(cos(beta).^2+((he/hx)^2).*sin(beta).^2).*cos(lambda);
 y = ay.*cos(beta).*sin(lambda);
 z = b.*sin(beta).*sqrt(1-((he/hx)^2).*cos(lambda).^2);
+
+geodeticPathCoords = [x y z];
+
 plot3(x,y,z)
 
 B = (ay^2.*sin(beta).^2+b^2.*cos(beta).^2)./(hx^2-hy^2.*sin(beta).^2);
@@ -181,7 +231,7 @@ s = (h/3)*sum(S*Fv);
 end
 
 
-function [n,c,a0,a1,s] = Geodesics(ax,ay,b,beta0,lambda0,beta1,lambda1,Sub,epsilon)
+function [n,c,a0,a1,s,geodeticPathCoords] = Geodesics(ax,ay,b,beta0,lambda0,beta1,lambda1,Sub,epsilon)
 
 %----- Ellipsoid -----
 hx = sqrt(ax^2-b^2);
@@ -205,30 +255,30 @@ ddbeta0 = 0;
 
 n=1;
 while n<100
-n;
-
-Dbeta0 = dbeta0;
-Ddbeta0 = ddbeta0;
-
-R = RK4(lambda0, lambda1, Sub, [beta0,Dbeta0+Ddbeta0,0,1], ax, ay, b);
-
-lambda = R(:,1);
-beta = R(:,2);
-dbeta = R(:,3);
-pbeta = R(:,4);
-pdbeta = R(:,5);
-
-beta10 = beta(end);
-pbeta10 = pbeta(end);
-
-dbeta0 = dbeta(1);
-ddbeta0 = (beta1-beta10)/pbeta10;
-
-%Check
-n = n+1;
-if abs(beta1-beta10) < epsilon
-   break
-end
+    n;
+    
+    Dbeta0 = dbeta0;
+    Ddbeta0 = ddbeta0;
+    
+    R = RK4(lambda0, lambda1, Sub, [beta0,Dbeta0+Ddbeta0,0,1], ax, ay, b);
+    
+    lambda = R(:,1);
+    beta = R(:,2);
+    dbeta = R(:,3);
+    pbeta = R(:,4);
+    pdbeta = R(:,5);
+    
+    beta10 = beta(end);
+    pbeta10 = pbeta(end);
+    
+    dbeta0 = dbeta(1);
+    ddbeta0 = (beta1-beta10)/pbeta10;
+    
+    %Check
+    n = n+1;
+    if abs(beta1-beta10) < epsilon
+        break
+    end
 end
 %----------
 
@@ -236,6 +286,8 @@ x = ax.*sqrt(cos(beta).^2+((he/hx)^2).*sin(beta).^2).*cos(lambda);
 y = ay.*cos(beta).*sin(lambda);
 z = b.*sin(beta).*sqrt(1-((he/hx)^2).*cos(lambda).^2);
 plot3(x,y,z)
+geodeticPathCoords = [x y z];
+
 
 B = (ay^2.*sin(beta).^2+b^2.*cos(beta).^2)./(hx^2-hy^2.*sin(beta).^2);
 L = (ax^2.*sin(lambda).^2+ay^2.*cos(lambda).^2)./(hx^2-he^2.*cos(lambda).^2);
@@ -436,12 +488,12 @@ beta(1) = beta0;
 w(:,1) = x0;                    %initial conditions
 
 for i = 1:Sub
-   k1 = h*F_dbeta(beta(i), w(:,i), ax,ay,b);
-   k2 = h*F_dbeta(beta(i)+0.5*h, w(:,i)+0.5*k1, ax,ay,b);
-   k3 = h*F_dbeta(beta(i)+0.5*h, w(:,i)+0.5*k2, ax,ay,b);
-   k4 = h*F_dbeta(beta(i)+h, w(:,i)+k3, ax,ay,b);
-   w(:,i+1) = w(:,i) + (k1 + 2*k2 + 2*k3 + k4)/6;
-   beta(i+1) = beta0 + i*h;
+    k1 = h*F_dbeta(beta(i), w(:,i), ax,ay,b);
+    k2 = h*F_dbeta(beta(i)+0.5*h, w(:,i)+0.5*k1, ax,ay,b);
+    k3 = h*F_dbeta(beta(i)+0.5*h, w(:,i)+0.5*k2, ax,ay,b);
+    k4 = h*F_dbeta(beta(i)+h, w(:,i)+k3, ax,ay,b);
+    w(:,i+1) = w(:,i) + (k1 + 2*k2 + 2*k3 + k4)/6;
+    beta(i+1) = beta0 + i*h;
 end
 
 R = [beta' w'];
@@ -457,12 +509,12 @@ lambda(1) = lambda0;
 w(:,1) = x0;                    %initial conditions
 
 for i = 1:Sub
-   k1 = h*F(lambda(i), w(:,i), ax,ay,b);
-   k2 = h*F(lambda(i)+0.5*h, w(:,i)+0.5*k1, ax,ay,b);
-   k3 = h*F(lambda(i)+0.5*h, w(:,i)+0.5*k2, ax,ay,b);
-   k4 = h*F(lambda(i)+h, w(:,i)+k3, ax,ay,b);
-   w(:,i+1) = w(:,i) + (k1 + 2*k2 + 2*k3 + k4)/6;
-   lambda(i+1) = lambda0 + i*h;
+    k1 = h*F(lambda(i), w(:,i), ax,ay,b);
+    k2 = h*F(lambda(i)+0.5*h, w(:,i)+0.5*k1, ax,ay,b);
+    k3 = h*F(lambda(i)+0.5*h, w(:,i)+0.5*k2, ax,ay,b);
+    k4 = h*F(lambda(i)+h, w(:,i)+k3, ax,ay,b);
+    w(:,i+1) = w(:,i) + (k1 + 2*k2 + 2*k3 + k4)/6;
+    lambda(i+1) = lambda0 + i*h;
 end
 
 R = [lambda' w'];
