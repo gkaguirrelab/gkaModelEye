814ba371697866df9304a63b55944bb1de6a6de4
gkaguirre <gkaguirre@me.com>
2018-07-21 12:00:05 -0400

    Added this function
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- +quadric/angleRays.m
+++ +quadric/angleRays.m
@@ -1,8 +1,8 @@
-function [angle, angle_p1p2, angle_p1p3] = angleRays( R1, R2 )
+function [angle, angle_xy, angle_xz] = angleRays( R1, R2 )
 % Returns the angle between two rays
 %
 % Syntax:
-%  R = quadric.normalizeRay( R )
+%  R = quadric.angleRays( R )
 %
 % Description:
 %   Just what it says on the tin.
@@ -15,16 +15,32 @@ function [angle, angle_p1p2, angle_p1p3] = angleRays( R1, R2 )
 %
 % Outputs:
 %   angle                 - Scalar. Angle in degrees between the rays.
+%   angle_xy, angle_xz    - Scalars. Angles in degrees between the rays
+%                           projected on the xy and xz planes.
 %
 % Examples:
 %{
-    p=[0;0;0];
-    u=[4;1;1];
-    R=[p,u];
-    R=quadric.normalizeRay(R);
+    p = [0;0;0];
+    u = [1;0;0];
+    R1 = quadric.normalizeRay([p, u]);
+    p = [0;0;0];
+    u = [1;tand(15);tand(-7)];
+    R2 = quadric.normalizeRay([p, u]);
+    [angle, angle_xy, angle_xz] = quadric.angleRays( R1, R2 )
 %}
 
+% Calculate the overall angle between the two vectors
 angle = rad2deg(atan2(norm(cross(R1(:,2),R2(:,2))), dot(R1(:,2),R2(:,2))));
+
+% Now obtain the angles as projected on the xy and xz planes
+u1_xy = [R1(1,2);R1(2,2);0];
+u2_xy = [R2(1,2);R2(2,2);0];
+angle_xy = rad2deg(atan2(norm(cross(u1_xy,u2_xy)), dot(u1_xy,u2_xy)));
+
+u1_xz = [R1(1,2);0;R1(3,2)];
+u2_xz = [R2(1,2);0;R2(3,2)];
+angle_xz = rad2deg(atan2(norm(cross(u1_xz,u2_xz)), dot(u1_xz,u2_xz)));
+
 
 end
 
