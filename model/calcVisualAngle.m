b1bafde63f2ca3336210c3d4418cbcf5d927aebc
gkaguirre <gkaguirre@me.com>
2018-07-29 11:32:16 -0400

    Handle spectral state
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- model/calcVisualAngle.m
+++ model/calcVisualAngle.m
@@ -1,4 +1,4 @@
-function visualAngles = calcVisualAngle(eye,G0,G1,X0,X1)
+function visualAngles = calcVisualAngle(eye,G0,G1,X0,X1,cameraMedium)
 % The visual angles between two retinal points
 %
 % Syntax:
@@ -6,7 +6,7 @@ function visualAngles = calcVisualAngle(eye,G0,G1,X0,X1)
 %
 % Description
 %   Given a sceneGeometry and two coordinates on the retinal surface, the
-%   routine returns a vector that contains the visual angle (in degrees) 
+%   routine returns a vector that contains the visual angle (in degrees)
 %   between the two points, projected on the p1p2 and p1p3 planes (i.e.,
 %   horizontal and vertical visual angle).
 %
@@ -23,16 +23,18 @@ function visualAngles = calcVisualAngle(eye,G0,G1,X0,X1)
 %                           this solution is only defined on the surface.
 %   X0, X1                - 3x1 vectors that specify the Cartesian
 %                           location of points on the quadric surface.
+%   cameraMedium          - The medium in which the eye is located.
+%                           Defaults to 'air'.
 %
 % Outputs:
-%   visualAngles          - 1x2 vector with the visual angle, in degrees 
+%   visualAngles          - 1x2 vector with the visual angle, in degrees
 %                           between the two points within the p1p2 and p1p3
 %                           planes.
 %
 % Examples:
 %{
     % Display a map of visual angle on the retinal surface
-    eye = modelEyeParameters();
+    eye = modelEyeParameters('eyeLaterality','os');
     S = eye.retina.S;
     boundingBox = eye.retina.boundingBox;
     figure
@@ -44,7 +46,7 @@ function visualAngles = calcVisualAngle(eye,G0,G1,X0,X1)
     % and obtain the visual angle and each point w.r.t. the fovea.
     c = jet();
     nColors = size(c,1);
-    for beta = -90:3:30
+    for beta = -90:3:40
         for omega = -180:5:180
             coord = quadric.ellipsoidalGeoToCart( [beta, omega, 0], S );
             visualAngles = calcVisualAngle(eye,eye.axes.visual.geodetic,[beta omega 0]);
@@ -65,12 +67,43 @@ function visualAngles = calcVisualAngle(eye,G0,G1,X0,X1)
     plot3(eye.axes.opticDisc.coords(1),eye.axes.opticDisc.coords(2),eye.axes.opticDisc.coords(3),'ok','MarkerSize',10);
 %}
 
-% If only three input values were passed, derive the X0/X1 Cartesian
-% coordinates from the ellipsoidal geodetic coordinates.
+
+if nargin<=2
+    error('Invalid number of input arguments');
+end
+
 if nargin==3
+    % If only three input values were passed, derive the X0/X1 Cartesian
+    % coordinates from the ellipsoidal geodetic coordinates.
     S = eye.retina.S;
     X0 = quadric.ellipsoidalGeoToCart( G0, S )';
     X1 = quadric.ellipsoidalGeoToCart( G1, S )';
+    cameraMedium = 'air';
+end
+
+if nargin==4
+    error('Invalid number of input arguments');
+end
+
+if nargin==5
+    % Check if the X0/X1 values are empty. If so, derive the X0/X1
+    % Cartesian coordinates from the ellipsoidal geodetic coordinates.
+    if isempty(X0)
+        S = eye.retina.S;
+        X0 = quadric.ellipsoidalGeoToCart( G0, S )';
+        X1 = quadric.ellipsoidalGeoToCart( G1, S )';
+    end
+    cameraMedium = 'air';
+end
+
+if nargin==6
+    % Check if the X0/X1 values are empty. If so, derive the X0/X1
+    % Cartesian coordinates from the ellipsoidal geodetic coordinates.
+    if isempty(X0)
+        S = eye.retina.S;
+        X0 = quadric.ellipsoidalGeoToCart( G0, S )';
+        X1 = quadric.ellipsoidalGeoToCart( G1, S )';
+    end
 end
 
 % Handle to the virtual image function; use the MEX version if available
@@ -86,7 +119,7 @@ end
 % coordinates.
 args = {eye.pupil.center([2 3 1])', ...
     eye.rotationCenters, ...
-    assembleOpticalSystem( eye, 'surfaceSetName','retinaToPupil' )};
+    assembleOpticalSystem( eye, 'surfaceSetName','retinaToPupil', 'cameraMedium', cameraMedium )};
 
 % Set eyePose to all zeros (i.e., the eye is not rotated).
 eyePose = [0 0 0 0];
@@ -103,8 +136,8 @@ R1 = quadric.normalizeRay(R1');
 R1(:,2)=-R1(:,2);
 
 % Ray trace from the pupil center through the cornea
-R0 = rayTraceQuadrics(R0, assembleOpticalSystem( eye, 'surfaceSetName','pupilToCamera','cameraMedium','air' ));
-R1 = rayTraceQuadrics(R1, assembleOpticalSystem( eye, 'surfaceSetName','pupilToCamera','cameraMedium','air' ));
+R0 = rayTraceQuadrics(R0, assembleOpticalSystem( eye, 'surfaceSetName','pupilToCamera','cameraMedium',cameraMedium ));
+R1 = rayTraceQuadrics(R1, assembleOpticalSystem( eye, 'surfaceSetName','pupilToCamera','cameraMedium',cameraMedium ));
 
 % Calculate and return the signed angles between the two rays
 [~, angle_p1p2, angle_p1p3] = quadric.angleRays( R0, R1 );
