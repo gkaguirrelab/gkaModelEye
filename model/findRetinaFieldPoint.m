9fc6a43beeecd76fdb421dedaa9ad5a570273e7c
gkaguirre <gkaguirre@me.com>
2019-03-09 08:38:46 -0500

    Closer
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- model/findRetinaFieldPoint.m
+++ model/findRetinaFieldPoint.m
@@ -20,8 +20,8 @@ function [G,X,angleError] = findRetinaFieldPoint( eye, degField, cameraMedium )
 %{
     sceneGeometry = createSceneGeometry();
     degField = [5.8 3.0 0];
-    G = findRetinaFieldPoint( sceneGeometry.eye, degField);
-    [outputRay,rayPath] = calcNodalRay(eye,G)
+    [G,X,angleError] = findRetinaFieldPoint( sceneGeometry.eye, degField);
+    [outputRay,rayPath] = calcNodalRay(eye,G);
     plotOpticalSystem('surfaceSet',sceneGeometry.refraction.retinaToCamera,'addLighting',true,'rayPath',rayPath,'outputRay',outputRay);
 %}
 
@@ -37,18 +37,20 @@ opts = optimoptions(@fmincon,'Algorithm','interior-point','Display','off');
 
 % To set an x0 guess, identify the retinal point that is at the specified
 % angle w.r.t. to the optical axis and the center of the aperture stop.
-R = rayFromAngles([-5.4 0 0]',degField(1),degField(2));
+R = rayFromAngles([-5.4 0 0]',-degField(2),-degField(1));
 x0 = quadric.intersectRay(S,R,eye.retina.side,eye.retina.boundingBox);
 
-% Convert the x0 zero guess into geodetic coordinates
+% Convert the x0 zero guess into ellipsoidal geodetic coordinates
 g0 = quadric.cartToEllipsoidalGeo( x0, S );
+
+% Set the elev
 
 % Define the optical axis ray
 opticalAxis = [0 1; 0 0; 0 0];
 
 % The objective is to match the angles between the output nodal ray and the
 % optical axis to the specified degField angles
-myObj = @(G) sqrt(sum((degField(1:2)-quadric.angleRays(calcNodalRay(eye,G,[],cameraMedium),opticalAxis)).^2));
+myObj = @(G) sqrt(sum((degField(1:2).*[-1 -1]-wrapAngleRays(calcNodalRay(eye,G,[],cameraMedium),opticalAxis)).^2));
 
 % Set the bounds
 lb = [-90 -180 0]';
@@ -63,6 +65,11 @@ X = quadric.ellipsoidalGeoToCart(G,S)';
 end
 
 %% LOCAL FUNCTION
+
+% Wrap 
+function angles = wrapAngleRays(R0,R1)
+[~, angles(1), angles(2) ] = quadric.angleRays(R0,R1);
+end
 
 % Converts angles relative to the optical axis to a unit vector ray.
 function R = rayFromAngles(p,angle_p1p2,angle_p1p3)
