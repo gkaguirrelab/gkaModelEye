5e6dc4adbcd8e5859fa43445c0be1798c2ba7597
gkaguirre <gkaguirre@me.com>
2020-01-26 15:07:26 -0500

    Remove coder support as we are not using this
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- ellipse/pupilEllipseFit.m
+++ ellipse/pupilEllipseFit.m
@@ -35,43 +35,14 @@ validPerimIdx = find(~any(isnan(imagePoints)')');
 if ~isreal(imagePoints) || length(validPerimIdx)<5
     pupilEllipseOnImagePlane=nan(1,5);
 else
-    % Silence a warning that can arise regarding a nearly singular matrix
-    
+    % Silence warnings that can arise during fitting
     warnState = warning;
     warning('off','MATLAB:nearlySingularMatrix');
     warning('off','MATLAB:singularMatrix');
     
     % We place the ellipse fit in a try-catch block, as the fit can fail
-    % when the ellipse is so eccentric that it approaches a line. Try-catch
-    % is not supported in compiled code, so we detect this situation and
-    % proceed without the safety net.
-    if isempty(coder.target)
-        try
-            % Ellipse fitting with routine from the quadfit toolbox
-            implicitEllipseParams = real(ellipsefit_direct( imagePoints(validPerimIdx,1), imagePoints(validPerimIdx,2)));
-            % Convert the ellipse from implicit to transparent form
-            pupilEllipseOnImagePlane = ellipse_ex2transparent(ellipse_im2ex(implicitEllipseParams));
-            % Place theta within the range of 0 to pi
-            if pupilEllipseOnImagePlane(5) < 0
-                pupilEllipseOnImagePlane(5) = pupilEllipseOnImagePlane(5)+pi;
-            end
-            % Get the error of the ellipse fit to the pupil points
-            pupilFitError = sqrt(nanmean(ellipsefit_distance( imagePoints(validPerimIdx,1), imagePoints(validPerimIdx,2),ellipse_transparent2ex(pupilEllipseOnImagePlane)).^2));
-        catch ME
-            % If the ellipse fit direct fails because of an inability to
-            % fit an ellipse to the provided points, return nans and issue
-            % a warning.
-            pupilEllipseOnImagePlane=nan(1,5);
-            pupilFitError = nan;
-            switch ME.identifier
-                case {'MATLAB:badsubscript','MATLAB:realsqrt:complexResult','MATLAB:expectedReal','MATLAB:quad2dproj:expectedFinite','MATLAB:eig:matrixWithNaNInf'}
-                    warning('pupilProjection_fwd:ellipseFitFailed','Could not fit a valid ellipse to the pupil points; returning nans.');
-                otherwise
-                    ME.identifier
-                    warning('pupilProjection_fwd:ellipseFitUnknownError','Undefined error during ellipse fitting to pupil perimeter; returning nans.');
-            end
-        end % try-catch block
-    else
+    % when the ellipse is so eccentric that it approaches a line.
+    try
         % Ellipse fitting with routine from the quadfit toolbox
         implicitEllipseParams = real(ellipsefit_direct( imagePoints(validPerimIdx,1), imagePoints(validPerimIdx,2)));
         % Convert the ellipse from implicit to transparent form
@@ -82,7 +53,22 @@ else
         end
         % Get the error of the ellipse fit to the pupil points
         pupilFitError = sqrt(nanmean(ellipsefit_distance( imagePoints(validPerimIdx,1), imagePoints(validPerimIdx,2),ellipse_transparent2ex(pupilEllipseOnImagePlane)).^2));
-    end
+    catch ME
+        % If the ellipse fit direct fails because of an inability to
+        % fit an ellipse to the provided points, return nans and issue
+        % a warning.
+        pupilEllipseOnImagePlane=nan(1,5);
+        pupilFitError = nan;
+        switch ME.identifier
+            case {'MATLAB:badsubscript','MATLAB:realsqrt:complexResult','MATLAB:expectedReal','MATLAB:quad2dproj:expectedFinite','MATLAB:eig:matrixWithNaNInf'}
+                warning('gkaModelEye:pupilEllipseFit','Could not fit a valid ellipse to the pupil points; returning nans.');
+            otherwise
+                ME.identifier
+                warning('gkaModelEye:pupilEllipseFit','Undefined error during ellipse fitting to pupil perimeter; returning nans.');
+        end
+    end % try-catch block
+    
     warning(warnState);
 end
+
 end % pupilEllipseFit
