54a9e8b9859402f99ca6c49c6d356f56cb499d3e
gkaguirre <gkaguirre@me.com>
2020-02-24 14:09:12 -0600

    Setting up some coordinate transforms
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- bench/findPupilRay.m
+++ bench/findPupilRay.m
@@ -285,11 +285,12 @@ if any(isnan(outputRayEyeWorld))
     return
 end
 
-% We assign the variable eyeCoordTarget the coordinates of the target after
-% conversion to eye coordinates. Then, the point is counter-rotated by the
-% eye pose, so that the eyeCoordTarget is in a position equivalent to if
-% the eye had rotated.
-eyeCoordTarget = convertWorldToEyeCoord(eyePose, worldTarget, rotationCenters);
+% Move the worldTarget into the eye coordinate space
+eyeCoord = convertWorldToEyeCoord(worldTarget);
+
+% Counter-rotate the tatget by the eye pose, so that the eyeCoordTarget is
+% in a position equivalent to if the eye had rotated.
+eyeCoordTarget = rotateEyeCoord(eyeCoord, -eyePose, rotationCenters);
 
 % Calculate the distance between the closest approach of the outputRay to
 % the target.
@@ -298,32 +299,3 @@ distance = quadric.distancePointRay(eyeCoordTarget',outputRayEyeWorld');
 end % calcTargetIntersectError
 
 
-
-function eyeCoord = convertWorldToEyeCoord(eyePose, worldCoord, rotationCenters)
-
-% We assign the variable eyeCoordTarget the coordinates of the target after
-% conversion to eye coordinates. Then, the point is counter-rotated by the
-% eye pose, so that the eyeCoordTarget is in a position equivalent to if
-% the eye had rotated.
-cameraRot = -eyePose;
-RotAzi = [cosd(cameraRot(1)) -sind(cameraRot(1)) 0; sind(cameraRot(1)) cosd(cameraRot(1)) 0; 0 0 1];
-RotEle = [cosd(-cameraRot(2)) 0 sind(-cameraRot(2)); 0 1 0; -sind(-cameraRot(2)) 0 cosd(-cameraRot(2))];
-RotTor = [1 0 0; 0 cosd(cameraRot(3)) -sind(cameraRot(3)); 0 sind(cameraRot(3)) cosd(cameraRot(3))];
-
-% Rearrange the worldTarget dimensions to switch from world to eye
-% coordinate space. This is now the eyeTarget or ET
-eyeCoord = worldCoord([3 1 2])';
-
-% Torsion
-eyeCoord=eyeCoord-rotationCenters.tor;
-eyeCoord = (RotTor*eyeCoord')';
-eyeCoord=eyeCoord+rotationCenters.tor;
-% Elevation
-eyeCoord=eyeCoord-rotationCenters.ele;
-eyeCoord = (RotEle*eyeCoord')';
-eyeCoord=eyeCoord+rotationCenters.ele;
-% Azimuth
-eyeCoord=eyeCoord-rotationCenters.azi;
-eyeCoord = (RotAzi*eyeCoord')';
-eyeCoord=eyeCoord+rotationCenters.azi;
-end
