04bc04844ee91c33dc3d4504a6f57b11a4e147f9
gkaguirre <gkaguirre@me.com>
2020-03-18 10:47:55 -0400

    Implementing the Nakayama equation for pseudoTorsion
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- eye/addPseudoTorsion.m
+++ eye/addPseudoTorsion.m
@@ -6,21 +6,22 @@ function eyePoseAdjusted = addPseudoTorsion(eyePose,primaryPosition)
 %
 % Description:
 %   Head-fixed eye movements generally obey Listing's Law, which observes
-%   that there is no change in the "true" torsion of the eye with a change
-%   in position. When eye rotations are implemented with quaternions, this
-%   property is automatically achieved. In this model eye system, however,
-%   we implement eye rotations as a series of rotations following the
-%   "Helmholtz coordinates". (The motivation for using a series of
-%   rotations instead of quaternions is that it allows us to implement
-%   separate rotation centers for each direction of eye rotation).
+%   that there is no change in the "true" torsion of the eye (with respect
+%   to its optical axis) with a change in position. When eye rotations are
+%   implemented with quaternions, this property is automatically achieved.
+%   In this model eye system, however, we implement eye rotations as a
+%   series of rotations following the "Fick coordinates". (The motivation
+%   for using a series of rotations instead of quaternions is that it
+%   allows us to implement separate rotation centers for each direction of
+%   eye rotation).
 %
 %   To create eye movements that obey Listing's Law, we therefore need to
 %   add "pseudo torsion" to the eyePose. This correction, and its
 %   theoretical motivation, is described in:
 %
-%       Wong, Agnes MF. "Listing's law: clinical significance and
-%       implications for neural control." Survey of ophthalmology 49.6
-%       (2004): 563-575.
+%       Nakayama, K., K. Ciuffreda, and C. Schor. "Kinematics of normal and
+%       strabismic eyes." Basic and Clinical Aspects of Binocular Vergence
+%       Movements (1983).
 %
 %   The calculation of pseudoTorsion is performed relative to the "primary
 %   position" of the eye, which is the position from which any other eye
@@ -44,11 +45,6 @@ function eyePoseAdjusted = addPseudoTorsion(eyePose,primaryPosition)
 %
 % Examples:
 %{
-    % Test that we recover the value reported by Wong 2004 in Fig 3
-    eyePose = [30  30 0 3];
-    primaryPosition = [0 0];
-    eyePoseAdjusted = addPseudoTorsion(eyePose,primaryPosition);
-    assert(max(abs(eyePoseAdjusted(3) - 7.9)));
 %}
 
 % Obtain the horizontal and vertical position of the eye relative to the
@@ -56,10 +52,15 @@ function eyePoseAdjusted = addPseudoTorsion(eyePose,primaryPosition)
 H = -(eyePose(1)-primaryPosition(1));
 V = -(eyePose(2)-primaryPosition(2));
 
-% Calculate the pseudoTorsion, following eq 1 of Wong 2004. This
-% calculation must be performed in radians, so there is some conversion
-% here.
-pseudoTorsion = rad2deg(-(deg2rad(H)*deg2rad(V))/2);
+% Calculate the pseudoTorsion, following eq alpha of Nakayama 1983.
+A = sind(H)*sind(V);
+B = 1+cos(V)*cos(H);
+
+% Need to avoid cases that return complex values
+pseudoTorsion = 0;
+if B~=0 && A<B
+   pseudoTorsion = -asind(A/B);
+end
 
 % Create the adjusted eyePose
 eyePoseAdjusted = eyePose;
