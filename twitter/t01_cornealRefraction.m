e9ced001f62cff6ac3d97b9dd081c2d80e708dfd
gkaguirre <gkaguirre@me.com>
2020-04-15 11:54:17 -0400

    Setting up some short, educational scripts to tweet about
    Signed-off-by: gkaguirre <gkaguirre@me.com>

---
+++ twitter/t01_cornealRefraction.m
@@ -0,0 +1,36 @@
+%% t01_cornealRefraction
+% Demonstrate the effect of refraction by the cornea on the appearance of
+% the entrance pupil of the eye
+
+
+% Create a sceneGeometry object that describes the eye, a camera, and their
+% position relative to one another
+sceneGeometry=createSceneGeometry();
+
+% Define an eyePose with the azimuth, elevation, torsion rotation of the 
+% eye in degrees, and the radius of the aperture stop of the iris in mm
+eyePose = [-30 -5 0 3];
+
+% These are the elements of the model eye that we will render
+modelEyeLabelNames = {'retina' 'irisPerimeter' 'pupilEllipse' 'cornea'};
+modelEyePlotColors = {'.w' 'ob' '-g' '.y'};
+
+
+% Render the eye with corneal refraction
+renderEyePose(eyePose, sceneGeometry, ...
+    'modelEyeLabelNames',modelEyeLabelNames,...
+    'modelEyePlotColors',modelEyePlotColors);
+
+% Remove the effect of corneal refraction. To do so, we will hack the
+% index of refraction of the aqueous humor, cornea, and tear film to all
+% be one.
+sceneGeometry.refraction.stopToMedium.opticalSystem(1:3,19) = 1;
+
+% Add the stop ellipse to the figure
+modelEyeLabelNames = {'pupilEllipse'};
+modelEyePlotColors = {'-r'};
+renderEyePose(eyePose, sceneGeometry, ...
+    'newFigure', false,...
+    'modelEyeLabelNames',modelEyeLabelNames,...
+    'modelEyePlotColors',modelEyePlotColors);
+title('Stop in red, pupil in green')
