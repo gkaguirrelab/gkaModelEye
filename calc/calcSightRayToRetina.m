794d32078c630b1888dc962bb6fb5e9ac5cd168c
gkaguirre <gkaguirre@me.com>
2021-02-19 11:30:49 -0500

    Renamed, and harmonized in style with other calc functions

--- calc/calcSightRayToRetina.m
+++ calc/calcSightRayToRetina.m
@@ -1,8 +1,8 @@
-function [rayPath,error] = calcSightRay(eye,G,X,stopRadius,originRayDepth,cameraMedium)
+function [rayPath,error] = calcSightRayToRetina(eye,rayDestination,rayOriginDistance,stopRadius,cameraMedium)
 % Ray that passes through entrance pupil center to reach the retinal coord
 %
 % Syntax:
-%  [outputRay,rayPath,fixationEyePose,foveaDistanceError] = calcLineOfSightRay(sceneGeometry,stopRadius,fixTargetDistance)
+%  [outputRay,rayPath,fixationEyePose,foveaDistanceError] = calcSightRayToRetina(sceneGeometry,stopRadius,fixTargetDistance)
 %
 % Description
 %   Given an eye and a retinal coordinate, the routine identifies the ray
@@ -18,17 +18,20 @@ function [rayPath,error] = calcSightRay(eye,G,X,stopRadius,originRayDepth,camera
 %
 % Inputs:
 %   eye                   - Structure. SEE: modelEyeParameters
-%   G                     - 3x1 vector that provides the geodetic
-%                           coordinates beta, omega, and elevation in units
-%                           of degrees. Beta is defined over the range
-%                           -90:90, and omega over the range -180:180.
-%                           Elevation has an obligatory value of zero as
-%                           this solution is only defined on the surface.
-%   X                     - 3x1 vector that specifies the Cartesian
-%                           location of a point on the quadric surface.
+%   rayDestination        - 1x3 or 3x1 vector that provides the coordinates
+%                           of a location on the retina. The location can
+%                           be given in Cartesian or geodetic coordinates.
+%                           For the latter, the values are beta, omega, and
+%                           elevation in units of degrees. Beta is defined
+%                           over the range -90:90, and omega over the range
+%                           -180:180. Elevation has an obligatory value of
+%                           zero as this solution is only defined on the
+%                           surface. The routine will attempt to determine
+%                           which type of coordinate set has been provided.
+%   rayOriginDistance     - Scalar. The distance (in mm) of the origin of
+%                           the ray from the corneal apex. Assumed to be
+%                           500 mm if not defined.
 %   stopRadius            - Scalar. Radius of the aperture stop, in mm.
-%   originRayDepth        - Scalar. The distance (in mm) of the origin of
-%                           the ray from the corneal apex.
 %   cameraMedium          - The medium in which the eye is located.
 %                           Defaults to 'air'.
 %
@@ -49,7 +52,7 @@ function [rayPath,error] = calcSightRay(eye,G,X,stopRadius,originRayDepth,camera
     % vertex
     G = [-65,-65,0];
     % Find the sight ray
-    [rayPath,error] = calcSightRay(eye,G);
+    [rayPath,error] = calcSightRayToRetina(eye,G);
     % Show the optical system and sight ray
     opticalSystem = assembleOpticalSystem(eye,'surfaceSetName','mediumToRetina');
     plotOpticalSystem('surfaceSet',opticalSystem,'addLighting',true,'rayPath',rayPath,'surfaceAlpha',0.05);
@@ -84,65 +87,82 @@ function [rayPath,error] = calcSightRay(eye,G,X,stopRadius,originRayDepth,camera
 % Parse inputs
 
 if nargin<2
-    error('Invalid number of input arguments');
+    error('calcSightRayToRetina:invalidArguments','Too few input arguments')
 end
 
 if nargin==2
-    X = [];
+    rayOriginDistance = 500;
     stopRadius = 1.53;
-    originRayDepth = 500;
     cameraMedium = 'air';
 end
 
 if nargin==3
     stopRadius = 1.53;
-    originRayDepth = 500;
     cameraMedium = 'air';
 end
 
 if nargin==4
-    originRayDepth = 500;
     cameraMedium = 'air';
 end
 
-if nargin==5
-    cameraMedium = 'air';
+% Make rayDestination a column vector
+if all(size(rayDestination)==[1 3])
+    rayDestination = rayDestination';
 end
 
-% Check if the X0 value is empty. If so, derive the X0
-% Cartesian coordinates from the ellipsoidal geodetic coordinate.
-if isempty(X)
-    S = eye.retina.S;
-    X = quadric.ellipsoidalGeoToCart( G, S );
-end
+% Time to interpret the rayDestination variable. Hard-code a tolerance for
+% the distance of the rayDestination coordinate from the retinal surface
+surfaceTol = 1e-6;
 
-% Make sure that X is a column vector
-if all(size(X)==[1 3])
-    X = X';
+% Get the retinal surface and quadric function
+S = eye.retina.S;
+Sfunc = quadric.vecToFunc(S);
+
+% Interpret the value as Cartesian, if it is not a valid retinal location,
+% the quadric function will evaluated with a non-zero value
+if Sfunc(rayDestination(1),rayDestination(2),rayDestination(3)) > surfaceTol
+    
+    % If the last value of the coordinate is not zero, then this can't be a
+    % geodetic coordinate either
+    if rayDestination(3) > surfaceTol
+        error('calcNodalRayToRetina:invalidCoordinate','Supply a Cartesian or geodetic coordinate that is on the retinal surface.')
+    end
+    
+    % Check that the candidate beta and omega values are in range
+    if abs(rayDestination(1))>90 || abs(rayDestination(2))>180
+        error('calcNodalRayToRetina:invalidCoordinate','Supply a Cartesian or geodetic coordinate that is on the retinal surface.')
+    end
+    
+    % Looks like a valid geodetic coordinate. Convert to Cartesian.
+    rayDestination = quadric.ellipsoidalGeoToCart( rayDestination, S );
+        
 end
 
 % Create a default sceneGeometry (which has a pinhole camera)
-sceneGeometry = createSceneGeometry('eye',eye,'cameraMedium',cameraMedium,'surfaceSetName',{'stopToMedium','mediumToCamera','mediumToRetina'});
+sceneGeometry = createSceneGeometry('eye',eye,...
+    'cameraMedium',cameraMedium,...
+    'surfaceSetName',{'stopToMedium','mediumToCamera','mediumToRetina'});
 
 % Define an objective, which is the distance of the point of intersection
 % of the ray in the retina from the target coordinate. The point of origin
 % on the ray is set to be at the p(1), p(2) horizontal and vertical
 % locations, with a depth that sets the Euclidean distance of the point
-% from the cornea equal to the originRayDepth variable.
-myObj = @(p) retinalDistance([sqrt(originRayDepth^2-p(1)^2-p(2)^2);p(1);p(2)],sceneGeometry,stopRadius,X);
+% from the cornea equal to the rayOriginDistance variable.
+myCoord = @(p) [sqrt(rayOriginDistance^2-p(1)^2-p(2)^2);p(1);p(2)];
+myObj = @(p) objective(myCoord(p),sceneGeometry,stopRadius,rayDestination);
 
 % Define an p0 location, which is in line with the retinal target and the
 % corneal apex
-p0 = (originRayDepth./X(1)) .* X(2:3);
+p0 = (rayOriginDistance./rayDestination(1)) .* rayDestination(2:3);
 
 % Perform the search, finding the [h,v] location of the ray origin
 p = fminsearch(myObj,p0);
 
 % Assemble the origin point of the ray
-T = [sqrt(originRayDepth^2-p(1)^2-p(2)^2);p(1);p(2)];
+rayOrigin = myCoord(p);
 
 % Perfrom the ray trace one more time and save the output for return
-[error, outputRay, rayPath] = retinalDistance(T,sceneGeometry,stopRadius, X);
+[error, outputRay, rayPath] = objective(rayOrigin,sceneGeometry,stopRadius,rayDestination);
 
 % Concatenate the outputRay onto the rayPath
 rayPath(:,end+1)=outputRay(:,1);
@@ -150,21 +170,22 @@ rayPath(:,end+1)=outputRay(:,1);
 
 end
 
+
 %% Local function
-function [fVal, outputRay, rayPath] = retinalDistance(T,sceneGeometry,stopRadius, X)
+function [fVal, outputRay, rayPath] = objective(rayOrigin,sceneGeometry,stopRadius, rayDestination)
 
 % Obtain the center of the entrance pupil as seen from T
-sceneGeometry.cameraPosition.translation = T([2 3 1]);
+sceneGeometry.cameraPosition.translation = rayOrigin([2 3 1]);
 [~, ~, ~, ~, ~, eyePoints, pointLabels] = projectModelEye([0 0 0 stopRadius], sceneGeometry, 'nStopPerimPoints', 16);
 entrancePupilCenter = mean(eyePoints(strcmp(pointLabels,'pupilPerimeter'),:))';
 
 % Trace a ray from T, towards the entrancePupilCenter, all the way to the
 % retina.
-R = quadric.normalizeRay([T,entrancePupilCenter-T]);
+R = quadric.normalizeRay([rayOrigin,entrancePupilCenter-rayOrigin]);
 [outputRay, rayPath] = rayTraceQuadrics(R, sceneGeometry.refraction.mediumToRetina.opticalSystem);
 
 % The error is the Euclidean distance between the retinal target coordinate
 % and the intersection of the ray upon the retina.
-fVal = norm(outputRay(:,1)-X);
+fVal = norm(outputRay(:,1)-rayDestination);
 
 end
