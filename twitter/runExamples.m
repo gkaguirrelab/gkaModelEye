487c33aa313d57d75f4f037bf5934c173ff7925b
gkaguirre <gkaguirre@me.com>
2021-02-19 19:29:40 -0500

    A function to run the twitter demos using the example test toolbox

---
+++ twitter/runExamples.m
@@ -0,0 +1,20 @@
+% runExamples
+%
+% This file invites the exampleTestToolbox to evaluate these demo
+%
+% Examples:
+%{
+    t01_cornealRefraction
+    t02_pupilWithRotation
+    t03_centersOfRotation
+    t04_aziElelRotation
+    t05_lineOfSight
+    t06_fixationPose
+    t07_torsion
+    t08_ListingsLaw
+    t09_rotationalAsymmetry
+    t10_glintStationary
+    t11_glintMoving
+    t12_cornealVariation
+    t13_eyePoseSearch
+%}
