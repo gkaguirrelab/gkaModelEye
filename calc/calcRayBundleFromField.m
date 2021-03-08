f58b035fbc58855ab6218fa7362a45d6d3857435
gkaguirre <gkaguirre@me.com>
2021-03-08 18:37:57 -0500

    Ray bundle through the entrance window

--- calc/calcRayBundleFromField.m
+++ calc/calcRayBundleFromField.m
@@ -1,36 +1,15 @@
-function [retinaPoints,rayPaths] = calcRayBundleFromField(opticalSystem,fieldAngularPosition,rayOriginDistance,angleReferenceCoord,distanceReferenceCoord,rayIntersectionHeight,cameraMedium)
-% Returns a nodal ray that arises from the specified field location
+function [retinaPoints,rayPaths] = calcRayBundleFromField(eye,fieldAngularPosition,stopRadius,rayOriginDistance,angleReferenceCoord,distanceReferenceCoord,nStopPerimPoints,paraxialThresh,cameraMedium)
+% Directs a bundle of rays at the entrance window of the eye
 %
 % Syntax:
-%  [rayPath,angleError] = calcNodalRayFromField(opticalSystem,fieldAngularPosition,rayOriginDistance,angleReferenceCoord,cameraMedium)
+%  [retinaPoints,rayPaths] = calcRayBundleFromField(eye,fieldAngularPosition,stopRadius,rayOriginDistance,angleReferenceCoord,distanceReferenceCoord,nStopPerimPoints,paraxialThresh,cameraMedium)
 %
 % Description
-%   
+%
 %
 % Inputs:
-%   opticalSystem         - Either an eye structure (from which a
-%                           "mediumToRetina" optical system in air will be
-%                           derived), or an mx19 matrix, where m is set by
-%                           the key value opticalSystemNumRows. Each row
-%                           contains the values:
-%                               [S side bb must n]
-%                           where:
-%                               S     - 1x10 quadric surface vector
-%                               side  - Scalar taking the value -1 or 1
-%                                       that defines which of the two
-%                                       points of intersection on the
-%                                       quadric should be used as the
-%                                       refractive surface.
-%                               bb    - 1x6 vector defining the bounding
-%                                       box within which the refractive
-%                                       surface is present.
-%                               must  - Scalar taking the value of 0 or 1,
-%                                       where 1 indicates that the ray must
-%                                       intersect the surface. If the ray
-%                                       misses a required surface, the
-%                                       routine exits with nans for the
-%                                       outputRay.
-%                               n     - Refractive index of the surface.
+%   eye                   - Structure. SEE: modelEyeParameters
+%   stopRadius            - Scalar. The size in mm of the aperture stop.
 %   fieldAngularPosition  - 2x1 vector that provides the coordinates of the
 %                           origin of the nodal ray in [horizontal,
 %                           vertical[ degrees with respect to the
@@ -47,81 +26,75 @@ function [retinaPoints,rayPaths] = calcRayBundleFromField(opticalSystem,fieldAng
 %                           The principal point is a typical choice. If not
 %                           defined, is set to [0;0;0], which is the origin
 %                           coordinate on the longitudinal axis.
+%   nStopPerimPoints      - Scalar. Then number of points on the stop
+%                           perimeter to be evaluated.
+%   paraxialThresh        - Scalar. The fieldRay will be forced to be
+%                           separated from the longitudinal axis by at
+%                           least this angle.
 %   cameraMedium          - String. The medium in which the eye is located.
 %                           Defaults to 'air'.
 %
 % Outputs:
-%   rayPath               - 3xm matrix that provides the ray coordinates
-%                           at each surface. The value for rayPath(1,:)
-%                           is equal to initial position. If a surface is
-%                           missed, then the coordinates for that surface
-%                           will be nan.
-%   angleError            - Scalar. The departure from parallel of the 
-%                           incident and emergent rays (deg)
+%   retinaPoints          - 3 x nStopPerimPoints. The set of points on the
+%                           retinal surface
+%   rayPaths              - Cell array of length nStopPerimPoints. Each
+%                           cell holds a ray path.
 %
 % Examples:
 %{
     % Define a default model eye
     accommodation = 1;
+    stopRadius = 1;
     eye = modelEyeParameters('accommodation',accommodation);
     % Pick a field location
     rayOriginDistance = 1000/accommodation;
     angleReferenceCoord = eye.landmarks.incidentNode.coords;
     distanceReferenceCoord = calcPrincipalPoint(eye);
-    fieldAngularPosition = [10 10];
+    fieldAngularPosition = [40 10];
     % Trace the bundle
-    retinaPoints = calcRayBundleFromField(eye,fieldAngularPosition,rayOriginDistance,angleReferenceCoord,distanceReferenceCoord);
-    % Confirm that the angleError is within tolerance
-    
+    [retinaPoints,rayPaths] = calcRayBundleFromField(eye,fieldAngularPosition,stopRadius,rayOriginDistance,angleReferenceCoord,distanceReferenceCoord);
+    % Compare this to the nodal ray point
+    nodalRayPath = calcNodalRayFromField(eye,fieldAngularPosition,rayOriginDistance,angleReferenceCoord,distanceReferenceCoord);
+    % Plot the optical system
+    opticalSystem = assembleOpticalSystem(eye,'surfaceSetName','mediumToRetina');
+    plotOpticalSystem('surfaceSet',opticalSystem,'addLighting',true,'surfaceAlpha', 0.05);
+    % Add the rays to the plot
+    for ii=1:length(rayPaths)
+        plotOpticalSystem('newFigure',false,'rayPath',rayPaths{ii});
+    end
+    xlim([-25 10]);
+    plot3(nodalRayPath(1,end),nodalRayPath(2,end),nodalRayPath(3,end),'*k');
 %}
 
 
 arguments
-    opticalSystem
+    eye (1,1) {isstruct}
     fieldAngularPosition (1,2) {mustBeNumeric} = [0,0]
+    stopRadius (1,1) {mustBeNumeric} = 2
     rayOriginDistance (1,1) {mustBeNumeric} = 1500
     angleReferenceCoord (3,1) {mustBeNumeric} = [0;0;0]
     distanceReferenceCoord (3,1) {mustBeNumeric} = [0;0;0]
-    rayIntersectionHeight (1,1) {mustBeNumeric} = 1
+    nStopPerimPoints (1,1) {mustBeNumeric} = 6
+    paraxialThresh (1,1) {mustBeNumeric} = 1e-3
     cameraMedium = 'air'
 end
 
 
-% Check if we were passed an eye model. If so, create the optical system
-if isstruct(opticalSystem)
-    if isfield(opticalSystem,'cornea')
-        eye = opticalSystem;
-        clear opticalSystem;
-        opticalSystem = assembleOpticalSystem(eye,...
-            'surfaceSetName','mediumToRetina','cameraMedium',cameraMedium);
-    end
-end
+% Create the optical system
+opticalSystem = assembleOpticalSystem(eye,...
+    'surfaceSetName','mediumToRetina','cameraMedium',cameraMedium);
 
+% Find the entrance window
+[~,objectCoord,entranceWindowPerimeter] = ...
+    calcEntranceWindow(eye,fieldAngularPosition,stopRadius,rayOriginDistance,angleReferenceCoord,distanceReferenceCoord,nStopPerimPoints,paraxialThresh,cameraMedium);
 
-% Define the primary fieldRay
-fieldRay = calcFieldRay(fieldAngularPosition,rayOriginDistance,angleReferenceCoord,distanceReferenceCoord);
-
-% Reverse the ray direction
-fieldRay(:,2) = -fieldRay(:,2);
-
-% Decompose the fieldRay
-rayOrigin = fieldRay(:,1);
-[p1p2, p1p3] = quadric.rayToAngles(fieldRay);
-
-% Loop over a set of displacement angles
-retinaPoints = [];
-rayPaths = {};
-bundleAngle = atand(rayIntersectionHeight/rayOriginDistance);
-nDivisions = 5;
-for hh = linspace(-bundleAngle,bundleAngle,nDivisions)
-    for vv = linspace(-bundleAngle,bundleAngle,nDivisions)
-        if norm([hh vv])>bundleAngle
-            continue
-        end
-        bundleRay = quadric.anglesToRay(rayOrigin,p1p2+hh,p1p3+vv);
-        [outputRay, rayPaths{end+1}] = rayTraceQuadrics(bundleRay, opticalSystem);
-        retinaPoints(:,end+1) = outputRay(:,1);
-    end
+% Direct rays from the field point to the borders of the entrance window
+retinaPoints = nan(3,nStopPerimPoints);
+rayPaths = cell(1,nStopPerimPoints);
+for ii = 1:size(entranceWindowPerimeter,2)
+    bundleRay = quadric.coordsToRay([objectCoord,entranceWindowPerimeter(:,ii)]);
+    [outputRay, rayPaths{ii}] = rayTraceQuadrics(bundleRay, opticalSystem);
+    retinaPoints(:,ii) = outputRay(:,1);
 end
 
 
