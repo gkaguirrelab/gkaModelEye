b99bf1925a1594753c74d59f2e2e4f74a261a9ae
gkaguirre <gkaguirre@me.com>
2021-03-21 10:03:59 -0400

    Move detaAngles up in param list

--- calc/calcMmRetinaPerDeg.m
+++ calc/calcMmRetinaPerDeg.m
@@ -1,8 +1,8 @@
-function mmPerDeg = calcMmRetinaPerDeg(eye,fieldAngularPosition,rayOriginDistance,angleReferenceCoord,distanceReferenceCoord,deltaDegEuclidean,cameraMedium)
+function mmPerDeg = calcMmRetinaPerDeg(eye,fieldAngularPosition,deltaAngles,rayOriginDistance,angleReferenceCoord,distanceReferenceCoord,cameraMedium)
 % Returns mm/degree of field angle at a field location
 %
 % Syntax:
-%  mmPerDeg = calcMmRetinaPerDeg(eye,fieldOrigin,deltaDegEuclidean,cameraMedium)
+%  mmPerDeg = calcMmRetinaPerDeg(eye,fieldAngularPosition,deltaAngles,rayOriginDistance,angleReferenceCoord,distanceReferenceCoord,cameraMedium)
 %
 % Description
 %   Given an eye structure and the location of a point in the field,
@@ -15,6 +15,12 @@ function mmPerDeg = calcMmRetinaPerDeg(eye,fieldAngularPosition,rayOriginDistanc
 %                           origin of the nodal ray in [horizontal,
 %                           vertical[ degrees with respect to the
 %                           coordinate specified in angleReferenceCoord.
+%   deltaAngles           - 1x2 vector. The measurement is made for a small
+%                           displacement of horizontal and vertical visual
+%                           angle specified by this variable. Setting this
+%                           value allows one to measure the unit conversion
+%                           in different polar directions around the
+%                           specified point.
 %   rayOriginDistance     - Scalar. The distance (in mm) of the origin of
 %                           the ray from the distanceReferenceCoord.
 %   angleReferenceCoord   - 3x1 vector that provides the coordinate from
@@ -27,9 +33,6 @@ function mmPerDeg = calcMmRetinaPerDeg(eye,fieldAngularPosition,rayOriginDistanc
 %                           The principal point is a typical choice. If not
 %                           defined, is set to [0;0;0], which is the origin
 %                           coordinate on the longitudinal axis.
-%   deltaDegEuclidean     - Scalar. The measurement is made for a small
-%                           displacement of visual angle specified by this
-%                           variable.
 %   cameraMedium          - The medium in which the eye is located.
 %                           Defaults to 'air'.
 %
@@ -39,12 +42,18 @@ function mmPerDeg = calcMmRetinaPerDeg(eye,fieldAngularPosition,rayOriginDistanc
 %
 % Examples:
 %{
+    % Simple example for the Drasdo model eye
+    eye = modelEyeParameters('species','Drasdo');
+    mmPerDeg = calcMmRetinaPerDeg(eye);
+%}
+%{
     % mm of retina / deg at the fovea
     eye = modelEyeParameters();
     fieldAngularPosition = eye.landmarks.fovea.degField;
+    deltaAngles = [1 0]; % Measure horizontally separated points
     rayOriginDistance = 1500;
     angleReferenceCoord = eye.landmarks.incidentNode.coords;
-    mmPerDeg = calcMmRetinaPerDeg(eye,fieldAngularPosition,rayOriginDistance,angleReferenceCoord);
+    mmPerDeg = calcMmRetinaPerDeg(eye,fieldAngularPosition,deltaAngles,rayOriginDistance,angleReferenceCoord);
     fprintf('%2.3f retinal mm per deg visual field at the fovea in the emmetropic eye.\n',mmPerDeg);
 %}
 
@@ -52,16 +61,13 @@ function mmPerDeg = calcMmRetinaPerDeg(eye,fieldAngularPosition,rayOriginDistanc
 arguments
     eye (1,1) {isstruct}
     fieldAngularPosition (1,2) {mustBeNumeric} = [0 0]
+    deltaAngles (1,2) {mustBeNumeric} = [1/sqrt(2) 1/sqrt(2)]
     rayOriginDistance {isscalar,mustBeNumeric} = 1500
     angleReferenceCoord (3,1) {mustBeNumeric} = [0;0;0]
     distanceReferenceCoord (3,1) double = [0;0;0]
-    deltaDegEuclidean {isscalar,mustBeNumeric} = 2
     cameraMedium = 'air'
 end
 
-
-% Set up the jitter in angles around the specified field location
-deltaAngles = [deltaDegEuclidean 0];
 
 % Trace the nodal rays from this field position
 rayPath0 = calcNodalRayFromField(eye,fieldAngularPosition-deltaAngles/2,rayOriginDistance,angleReferenceCoord,distanceReferenceCoord,cameraMedium);
